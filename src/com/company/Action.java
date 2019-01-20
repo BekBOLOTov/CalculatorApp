52f6b0a204f24359d9f5a6b8aed9aaadc61c7f84
Bolot Bekbolotov <bekbolotov.bolot@gmail.com>
2019-01-20 13:13:45 +0600

    Revert "Added point action and result action"
    This reverts commit 5aace6217b39f5eee6a2449e2d2e2d0726b32d99.

--- src/com/company/Action.java
+++ src/com/company/Action.java
@@ -2,7 +2,6 @@ package com.company;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.List;
 
