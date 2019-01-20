5aace6217b39f5eee6a2449e2d2e2d0726b32d99
Bolot Bekbolotov <bekbolotov.bolot@gmail.com>
2019-01-20 13:13:35 +0600

    Added point action and result action

--- src/com/company/Action.java
+++ src/com/company/Action.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.List;
 
